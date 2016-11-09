6c582b0f8a211c5f632c42f5202e6da972bd5614
fangxiaoyu <505558491@qq.com>
2016-11-09 22:50:55 +0800

    8e

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 import os
-from app import create_app,db
-from app.models import User,Role
+from app import create_app, db
+from app.models import User, Role
 from flask.ext.script import Manager,Shell
 from flask.ext.migrate import Migrate,MigrateCommand
 
