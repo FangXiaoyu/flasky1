c04af6d97a689fb49f5611deaf3e1907a8c4881a
FangXiaoyu <505558491@qq.com>
2017-02-21 20:07:32 +0800

    add ping() to upgrade last_seen time

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,6 +18,14 @@ def before_request():
         return redirect(url_for('auth.unconfirmed'))
 
 
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
+            and request.endpoint[:5]!= 'auth.':
+            return redirect(url_for('auth.unconfirmed'))
+
 @auth.route('/unconfirmed')
 def unconfirmed():
     if current_user.is_anonymous or current_user.confirmed:
--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
+from datetime import datetime
 
 
 class Permission:
@@ -50,6 +51,12 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    location = db.Column(db.String(64))
+    member_since = db.Column(db.DateTime(), default = datetime.utcnow)
+    last_seen = db.Column(db.DateTime, default = datetime.utcnow)
+
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -132,6 +139,10 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
 
 
 class AnonymousUser(AnonymousUserMixin):
